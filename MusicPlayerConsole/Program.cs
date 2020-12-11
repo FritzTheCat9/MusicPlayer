bb4e93acab817811dc67204daf6f7b398f1be35f
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2020-12-11 12:25:39 +0100

    zmiana

--- MusicPlayerConsole/Program.cs
+++ MusicPlayerConsole/Program.cs
@@ -24,12 +24,10 @@ namespace MusicPlayerConsole
                 ImportPlaylistFromXML(@"C:\Users\bartl\Desktop\playlist1.xml",
                 @"C:\Users\bartl\Desktop\piosenki i obrazki", @"C:\Users\bartl\Desktop\piosenki i obrazki");*/
 
-            /*MusicPlayer.getInstance().DownloadImageFromYoutubeVideo(@"https://www.youtube.com/watch?v=5zIXwpDsLF0&ab_channel=Jacu%C5%9B");
-            MusicPlayer.getInstance().DownloadSongFromYoutubeVideo(@"https://www.youtube.com/watch?v=5zIXwpDsLF0&ab_channel=Jacu%C5%9B");*/
-
             /*MusicPlayer.getInstance().SaveSongFromYoutube(@"https://www.youtube.com/watch?v=5zIXwpDsLF0&ab_channel=Jacu%C5%9B");
             MusicPlayer.getInstance().SaveSongFromYoutube(@"https://www.youtube.com/watch?v=vMLk_T0PPbk&ab_channel=BlackEyedPeasVEVO");*/
-            MusicPlayer.getInstance().SaveSongFromYoutube(@"https://www.youtube.com/watch?v=fZ3QyttXaaQ&ab_channel=Jacu%C5%9B");
+            /*MusicPlayer.getInstance().SaveSongFromYoutube(@"https://www.youtube.com/watch?v=fZ3QyttXaaQ&ab_channel=Jacu%C5%9B");
+            MusicPlayer.getInstance().SaveSongFromYoutube(@"https://www.youtube.com/watch?v=r4P-WOOUPk4&ab_channel=littlemixVEVO");*/
         }
 
     }
